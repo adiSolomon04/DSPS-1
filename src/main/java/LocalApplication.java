169cdbb09bad022dfa174b7643e81e65eae251a3
Adi solomon <adi20782@gmail.com>
2021-04-11 13:33:03 +0300

    1.Check if manager exists 2.terminate Instances

--- src/main/java/LocalApplication.java
+++ src/main/java/LocalApplication.java
@@ -1,6 +1,10 @@
-import com.example.ec2.CreateInstance;
+import com.example.ec2.EC2Operations;
 import com.example.s3.S3ObjectOperations;
 import software.amazon.awssdk.regions.Region;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Open s3 Bucket name: "dsps-s3-adieran-2021"
  */
@@ -9,15 +13,24 @@ public class LocalApplication {
     private static Region region = Region.US_EAST_1;
 
     public static void main(String[] args) {
-        region = Region.US_EAST_1;
-        //S3ObjectOperations s3 = new S3ObjectOperations(region);
+        //S3ObjectOperations s3Operations = new S3ObjectOperations(region);
+        EC2Operations ec2Operations = new EC2Operations();
         //todo: test if opening multiple Local in 1 computer is working
 
         //Upload file to S3
         //s3.uploadFile("B000EVOSE4.txt");
 
         //Create Manager Instance
-        CreateInstance.createInstance("Manager");
+        List<String> instanceIds = new ArrayList<>();
+        if(!ec2Operations.ManagerExists()) {
+            String Id = ec2Operations.createInstance(ec2Operations.ManagerName);
+            instanceIds.add(Id);
+        }
+
+
+
+        //ec2Operations.deleteInstanceById(instanceIds);
+        //ec2Operations.deleteInstanceByName(ec2Operations.ManagerName);
 
     }
 
---
+++ src/main/java/com/example/ec2/EC2Operations.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.example.ec2;
+
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.*;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+import software.amazon.awssdk.regions.Region;
+ 
+/**
+ * Creates an EC2 instance
+ */
+public class EC2Operations {
+    //static final Logger logger = LoggerFactory.getLogger(CreateInstance.class);
+    private static String amiId = "ami-5b41123e";
+    private Ec2Client ec2;
+    private Integer runningCode = 16;
+    private Integer pendingCode = 0;
+    public static String ManagerName = "Manager";
+    //static String name = "adisolo ec2";
+
+    public EC2Operations(){
+        ec2 = Ec2Client.builder()
+                .region(Region.US_EAST_1)
+                .build();
+    }
+
+    public String createInstance(String name) {
+        //To run this example, supply an instance name and AMI image id
+
+        RunInstancesRequest runRequest = RunInstancesRequest.builder()
+                .instanceType(InstanceType.T2_MICRO)
+                .imageId(amiId)
+                .maxCount(1)
+                .minCount(1)
+                .userData(Base64.getEncoder().encodeToString("hi there".getBytes()))
+                .build();
+
+        RunInstancesResponse response = ec2.runInstances(runRequest);
+
+        String instanceId = response.instances().get(0).instanceId();
+
+        Tag tag = Tag.builder()
+                .key("Name")
+                .value(name)
+                .build();
+
+        CreateTagsRequest tagRequest = CreateTagsRequest.builder()
+                .resources(instanceId)
+                .tags(tag)
+                .build();
+
+        ec2.createTags(tagRequest);
+        System.out.printf(
+                "Successfully started EC2 instance %s based on AMI %s",
+                instanceId, amiId);
+
+        return instanceId;
+    }
+
+    public void deleteInstanceById(List<String> instanceIds){
+        if(instanceIds.isEmpty())
+            return;
+        //for (String instanceId : instanceIds)
+        //    if(instanceId=="0")
+        //        instanceIds.remove(instanceId);
+
+        ec2.terminateInstances(TerminateInstancesRequest.builder()
+                .instanceIds(instanceIds)
+                .build());
+    }
+    public void deleteInstanceByName(String name){
+        List<String> instanceIds = getInstanceId(name);
+        if(instanceIds.isEmpty())
+            return;
+        ec2.terminateInstances(TerminateInstancesRequest.builder()
+                .instanceIds(instanceIds)
+                .build());
+    }
+
+    public List<String> getInstanceId(String name){
+        DescribeInstancesRequest request = DescribeInstancesRequest.builder().build();
+        DescribeInstancesResponse response = ec2.describeInstances(request);
+        List<String> instanceIds = new ArrayList<>();
+        for(Reservation reservation : response.reservations())
+            for(Instance instance : reservation.instances())
+                for (Tag tag : instance.tags())
+                    if ((tag.value().equals(name)) && (instance.state().code() == runningCode || instance.state().code() == pendingCode)) {
+                        instanceIds.add(instance.instanceId());
+                    }
+        return instanceIds;
+
+    }
+
+    /*
+    check if There is only one manager //todo: make sure !!!!
+     */
+    public boolean ManagerExists() {
+        return !getInstanceId("Manager").isEmpty();
+    }
+}
